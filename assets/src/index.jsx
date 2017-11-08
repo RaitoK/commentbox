206a39b76fd92f40e969bc34b6d8ff16a904f2aa
babykick <babykick@163.com>
2017-11-08 15:08:08 +0800

    update

--- assets/src/index.jsx
+++ assets/src/index.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'mobx-react';
 import {Router, useRouterHistory} from 'react-router';
 import { AppContainer } from 'react-hot-loader';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+// import injectTapEventPlugin from 'react-tap-event-plugin';
 import {createHashHistory} from 'history';
 import AppRoutes from './AppRoutes';
 import commentStore from './stores/commentStore';
@@ -11,7 +11,7 @@ import './vendor.css';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+// injectTapEventPlugin();
 
 const hashHistory = useRouterHistory(createHashHistory)({queryKey: false});
 const stores = { commentStore };
